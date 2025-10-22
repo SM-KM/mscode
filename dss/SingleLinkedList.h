776bd9a829f765226c98bda89d4c119e0540a134
SMKM <msantiagovargas2@gmail.com>
2025-10-21 20:18:17 -0600

    added the full class declaration for the SingleLinkedList, im gonna implement it in the same header file as well for all the others because of the templates.

--- dss/SingleLinkedList.h
+++ dss/SingleLinkedList.h
@@ -2,12 +2,26 @@
 #define SINGLELINKEDLIST_H
 
 #include <concepts>
+#include <forward_list>
+#include <initializer_list>
 #include <iostream>
 #include <memory>
 
 namespace dss {
 
-template <typename T> class SingleLinkedList_Node {};
+template <typename T> struct SingleLinkedList_Node {
+  T data;
+  SingleLinkedList_Node* next;
+
+  SingleLinkedList_Node();
+  SingleLinkedList_Node(T data);
+  ~SingleLinkedList_Node();
+
+  SingleLinkedList_Node(const SingleLinkedList_Node& sln);
+  SingleLinkedList_Node& operator=(const SingleLinkedList_Node& sln);
+  SingleLinkedList_Node(SingleLinkedList_Node&& sln);
+  SingleLinkedList_Node& operator=(SingleLinkedList_Node&& sln);
+};
 
 template <typename T, class Allocator = std::allocator<T>, bool Debug = false>
 class SingleLinkedList {
@@ -17,7 +31,7 @@ private:
   SingleLinkedList_Node<T> tail_;
 
 public:
-  // Custom iterator
+  // Custom iterator -- maybe implement outside
   struct iterator {
     T m_ptr;
 
@@ -63,17 +77,110 @@ public:
   using const_iterator = const iterator;
   using size_type = size_t;
 
-  // Methods
-  iterator begin() { return iterator(&root_); }
-  iterator end() { return iterator(nullptr); }
+  // ctors, dtor
+  SingleLinkedList() : SingleLinkedList(Allocator()) {};
+  explicit SingleLinkedList(const Allocator&);
+  explicit SingleLinkedList(size_type n, const Allocator& = Allocator());
+  SingleLinkedList(std::initializer_list<T>);
+  SingleLinkedList(size_type n, const T& value, const Allocator& = Allocator());
+  ~SingleLinkedList();
+
+  // to create SingleLinkedList from other stl containers
+  template <typename InputIt>
+  SingleLinkedList(InputIt begin, InputIt end, const Allocator& = Allocator());
+
+  // copy
+  SingleLinkedList(const SingleLinkedList& other);
+  SingleLinkedList& operator=(const SingleLinkedList& other);
+
+  // move
+  SingleLinkedList(SingleLinkedList&& other);
+  SingleLinkedList& operator=(SingleLinkedList&& other);
+
+  // iterators
+  iterator begin() noexcept { return iterator(&root_); };
+  const_iterator cbegin() const noexcept;
+  const_iterator cend() const noexcept;
+  iterator end() noexcept;
+  iterator rbegin() noexcept;
+  iterator rend() noexcept;
+
+  size_t size();
+
+  // assign elements from other stl container, using iterators
+  template <typename InputIt> void assign(InputIt begin, InputIt end);
+  void assign(size_type n, const T& t);
+  void assign(std::initializer_list<T>);
+
+  [[nodiscard]] bool empty() const noexcept;
+  size_type max_size() const noexcept;
+
+  // element access
+  reference front();
+  const_reference front() const;
+
+  // modifiers
+  template <typename... Args>
+  reference emplace_front(Args&&... args); // think well how this works
+
+  void push_front(const T& x);
+  void push_front(T&& x);
+  void pop_front();
+  void clear() noexcept;
+
+  template <typename... Args>
+  iterator emplace_after(const_iterator position, Args&&... args);
+  iterator insert_after(const_iterator position, const T& x);
+  iterator insert_after(const_iterator position, T&& x);
+  iterator insert_after(const_iterator position, size_type n, const T& x);
+
+  iterator erase_after(const_iterator position);
+  iterator erase_after(const_iterator begin, const_iterator end);
+
+  // noexpect compares if the allocator passed to the SingleLinkedList to swap
+  // are equal
+  void swap(SingleLinkedList& sl) noexcept(
+      std::allocator_traits<Allocator>::is_always_equal::value);
+
+  void resize(size_type s);
+  void resize(size_type s,
+              const value_type& t); // if the rezised SingleLinkedList is
+                                    // smaller than s, elements with value_type
+                                    // are added untill the new size is matched
+
+  void splice_after(const_iterator position, SingleLinkedList& sl);
+  void splice_after(const_iterator position, SingleLinkedList&& sl);
+  void splice_after(const_iterator position, SingleLinkedList& sl,
+                    const_iterator i);
+  void splice_after(const_iterator position, SingleLinkedList&& sl,
+                    const_iterator i);
+  void splice_after(const_iterator position, SingleLinkedList& sl,
+                    const_iterator begin, const_iterator end);
+  void splice_after(const_iterator position, SingleLinkedList&& sl,
+                    const_iterator begin, const_iterator end);
 
   // Cool improvement C++20 enforce that the param passed to remove_if is
   // callable using std::predicate<T> that would throw an error before compile
   // time, so basically before it tries to call predicate and the thing cannot
   // be called
+  size_type remove(const T& t);
   template <typename Predicate> size_type remove_if(Predicate pred);
+
+  size_type unique(); // deletes all duplicated of elements, but not permantly
+                      // the memory still needs to be handled
+
+  template <typename BinaryPredicate>
+  size_type unique(BinaryPredicate binary_pred);
+
+  void merge(SingleLinkedList& sl);
+  void merge(SingleLinkedList&& sl);
+  template <typename Compare> void merge(SingleLinkedList& sl, Compare comp);
+  template <typename Compare> void merge(SingleLinkedList&& sl, Compare comp);
+
+  void sort();
+  template <typename Compare> void sort(Compare comp);
+  void reverse() noexcept;
 };
 
 } // namespace dss
-
 #endif // SINGLELINKEDLIST_H
