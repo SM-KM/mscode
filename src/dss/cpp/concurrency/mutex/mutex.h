5e860d125fad1aadd9616ba5194e71a755b5c55d
SMKM <svargas@desma.com.mx>
2026-02-16 18:23:52 -0600

    definitions of mutexes

---
+++ src/dss/cpp/concurrency/mutex/mutex.h
@@ -0,0 +1,33 @@
+#ifndef MUTEX_H
+#define MUTEX_H
+
+namespace dss
+{
+
+class mutex
+{
+};
+
+class timed_mutex
+{
+};
+
+class lock_guard
+{
+};
+
+class unique_lock
+{
+};
+
+class scoped_lock
+{
+};
+
+class lock
+{
+};
+
+} // namespace dss
+
+#endif // MUTEX_H
