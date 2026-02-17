4fd47a187e1930f119f1bb596fc8a26dc2ad6565
SMKM <msantiagovargas2@gmail.com>
2026-02-16 19:58:26 -0600

    optional

---
+++ src/dss/cpp/optional/optional.h
@@ -0,0 +1,75 @@
+#ifndef OPTIONAL_H
+#define OPTIONAL_H
+
+#include <utility>
+
+namespace dss
+{
+
+struct nullopt_t
+{
+  constexpr explicit nullopt_t(int) {}
+};
+
+template <typename T>
+class optional
+{
+public:
+  using value_type = T;
+
+  constexpr optional() noexcept;
+  constexpr optional(dss::nullopt_t) noexcept;
+  constexpr optional(const optional& other);
+  constexpr optional(optional&& other) noexcept;
+
+  template <class U>
+  constexpr optional(const optional<U>& other);
+
+  template <class U>
+  constexpr optional(optional<U>&& other);
+
+  template <class... Args>
+  constexpr explicit optional(std::in_place_t, Args&&...args);
+
+  template <class U, class... Args>
+  constexpr explicit optional(std::in_place_t, std::initializer_list<U> ilist,
+                              Args&&...args);
+
+  template <class U = T>
+  constexpr optional(U&& value);
+
+  constexpr ~optional();
+
+  // operators
+  constexpr const T *operator->() const noexcept;
+  constexpr T *operator->() noexcept;
+  constexpr const T& operator*() const& noexcept;
+  constexpr T& operator*() & noexcept;
+  constexpr const T&& operator*() const&& noexcept;
+  constexpr T&& operator*() && noexcept;
+
+  constexpr T& value() &;
+  constexpr const T& value() const&;
+  constexpr T&& value() &&;
+  constexpr const T&& value() const&&;
+
+  // value or is the core of getting safely values from optional
+  template <class U>
+  constexpr T value_or(U&& default_value) const&;
+  template <class U>
+  constexpr T value_or(U&& default_value) &&;
+
+  constexpr void reset() noexcept;
+
+  // emplaceing the object on op
+  template <class... Args>
+  constexpr T& emplace(Args&&...args);
+  template <class U, class... Args>
+  constexpr T& emplace(std::initializer_list<U> ilist, Args&&...args);
+};
+
+// make optional
+
+} // namespace dss
+
+#endif // OPTIONAL_H
