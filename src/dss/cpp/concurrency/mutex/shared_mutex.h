b64005b3bf5b5e5ba8e30e846d43058106891129
SMKM <msantiagovargas2@gmail.com>
2026-02-16 19:23:52 -0600

    shared_mutex

--- src/dss/cpp/concurrency/mutex/shared_mutex.h
+++ src/dss/cpp/concurrency/mutex/shared_mutex.h
@@ -5,6 +5,17 @@ namespace dss
 {
 class shared_mutex
 {
+  shared_mutex();
+  shared_mutex(const shared_mutex&) = delete;
+  ~shared_mutex();
+
+  void lock();
+  void unlock();
+  [[nodiscard]] bool try_lock();
+
+  void lock_shared();
+  void unlock_shared();
+  [[nodiscard]] bool try_lock_shared();
 };
 
 class shared_lock
