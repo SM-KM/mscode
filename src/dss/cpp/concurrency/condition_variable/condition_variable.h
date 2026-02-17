ac3905efbf98de0317cefa7a066204975be34dae
SMKM <svargas@desma.com.mx>
2026-02-16 18:56:24 -0600

    wait cv

--- src/dss/cpp/concurrency/condition_variable/condition_variable.h
+++ src/dss/cpp/concurrency/condition_variable/condition_variable.h
@@ -1,3 +1,5 @@
+#include "../mutex/mutex.h";
+
 #ifndef CONDITION_VARIABLE_H
 #define CONDITION_VARIABLE_H
 
@@ -10,9 +12,12 @@ class condition_variable
   condition_variable(const condition_variable&) = delete;
   ~condition_variable();
 
-  // notifications to threads
   void notify_one() noexcept;
   void notify_all() noexcept;
+
+  void wait(dss::unique_lock<dss::mutex>& lock);
+  template <typename Predicate>
+  void wait(dss::unique_lock<dss::mutex>& lock, Predicate stop_waiting);
 };
 
 } // namespace dss
