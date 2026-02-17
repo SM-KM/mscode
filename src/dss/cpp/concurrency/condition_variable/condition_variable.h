6eece528383e2119c2bbea6022a86afba608f5ac
SMKM <msantiagovargas2@gmail.com>
2026-02-16 19:05:17 -0600

    wait until

--- src/dss/cpp/concurrency/condition_variable/condition_variable.h
+++ src/dss/cpp/concurrency/condition_variable/condition_variable.h
@@ -1,10 +1,17 @@
 #include "../mutex/mutex.h";
+#include <chrono>
 
 #ifndef CONDITION_VARIABLE_H
 #define CONDITION_VARIABLE_H
 
 namespace dss
 {
+
+enum class cv_status
+{
+  no_timeout,
+  timeout
+};
 
 class condition_variable
 {
@@ -18,6 +25,26 @@ class condition_variable
   void wait(dss::unique_lock<dss::mutex>& lock);
   template <typename Predicate>
   void wait(dss::unique_lock<dss::mutex>& lock, Predicate stop_waiting);
+
+  template <typename Rep, typename Period>
+  dss::cv_status wait_for(dss::unique_lock<dss::mutex>& lock,
+                          const std::chrono::duration<Rep, Period> rel_time);
+
+  template <typename Rep, typename Period, typename Predicate>
+  [[nodiscard]] bool wait_for(dss::unique_lock<dss::mutex>& lock,
+                              const std::chrono::duration<Rep, Period> rel_time,
+                              Predicate stop_waiting);
+
+  template <typename Clock, typename Duration>
+  dss::cv_status
+  wait_until(dss::unique_lock<dss::mutex>& lock,
+             const std::chrono::time_point<Clock, Duration> rel_time);
+
+  template <typename Clock, typename Duration, typename Predicate>
+  [[nodiscard]] bool
+  wait_until(dss::unique_lock<dss::mutex>& lock,
+             const std::chrono::time_point<Clock, Duration> rel_time,
+             Predicate stop_waiting);
 };
 
 } // namespace dss
