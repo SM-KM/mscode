e7c6608d63778c34c875ec34fadedf14ae88710d
SMKM <msantiagovargas2@gmail.com>
2026-03-03 13:31:54 -0600

    added support to use cmake to compile specific parts and testing with gtest

---
+++ src/dss/cpp/vector/tests/vector.cc
@@ -0,0 +1,3 @@
+#include <gtest/gtest.h>
+
+TEST(vector, constructor) { EXPECT_EQ(true, true); }
--- src/dss/cpp/vector/vector.h
+++ src/dss/cpp/vector/vector.h
@@ -11,19 +11,22 @@
 #include <type_traits>
 #include <vector>
 
-namespace dss
-{
-template <typename T, typename Allocator = std::allocator<T>>
-class vector
-{
+class A {
+  virtual void d();
+};
+class B : A {
+  virtual void d();
+};
+
+namespace dss {
+template <typename T, typename Allocator = std::allocator<T>> class vector {
   // considerations i want reference
   // access to iterators
-  class iterator
-  {
+  class iterator {
     using iterator_category = std::contiguous_iterator_tag;
     using difference_type = std::ptrdiff_t;
     using value_type = T;
-    using reference = T&;
+    using reference = T &;
     using pointer = T *;
 
   public:
@@ -32,74 +35,63 @@ class vector
 
     // Constructors
     constexpr iterator() = default;
-    constexpr explicit iterator(vector& vec_) : vec{vec_} {}
+    constexpr explicit iterator(vector &vec_) : vec{vec_} {}
 
     // Operators
-    constexpr T& operator*() { return *ptr; }
-    constexpr bool operator==(const iterator& other) const
-    {
+    constexpr T &operator*() { return *ptr; }
+    constexpr bool operator==(const iterator &other) const {
       return other.ptr == ptr;
     }
 
-    constexpr iterator& operator++()
-    {
+    constexpr iterator &operator++() {
       ++ptr;
       return *this;
     }
 
-    constexpr iterator operator++(int)
-    {
+    constexpr iterator operator++(int) {
       auto prev = *this;
       ++(*this);
       return prev;
     }
 
-    constexpr iterator& operator--()
-    {
+    constexpr iterator &operator--() {
       --ptr;
       return *this;
     }
 
-    constexpr iterator operator--(int)
-    {
+    constexpr iterator operator--(int) {
       auto prev = *this;
       ++(*this);
       return prev;
     }
 
-    constexpr iterator& operator+=(difference_type n)
-    {
+    constexpr iterator &operator+=(difference_type n) {
       ptr += n;
       return *this;
     }
 
-    constexpr iterator operator+(difference_type n) const
-    {
+    constexpr iterator operator+(difference_type n) const {
       auto next = *this;
       next += n;
       return next;
     }
 
-    constexpr iterator& operator-=(difference_type n)
-    {
+    constexpr iterator &operator-=(difference_type n) {
       ptr -= n;
       return *this;
     }
 
-    constexpr iterator operator-(difference_type n) const
-    {
+    constexpr iterator operator-(difference_type n) const {
       auto next = *this;
       next -= n;
       return next;
     }
 
-    difference_type operator-(const iterator& other) const
-    {
+    difference_type operator-(const iterator &other) const {
       return ptr - other.ptr;
     }
-    constexpr T& operator[](difference_type n) { return ptr[n]; }
-    constexpr auto operator<=>(const iterator& other)
-    {
+    constexpr T &operator[](difference_type n) { return ptr[n]; }
+    constexpr auto operator<=>(const iterator &other) {
       return other.ptr <=> ptr;
     }
     constexpr T *operator->() const { return ptr->data(); }
@@ -108,14 +100,13 @@ class vector
 public:
   using value_type = T;
 
-  template <typename U>
-  using type_identity_t = U;
+  template <typename U> using type_identity_t = U;
   using pointer = typename std::allocator_traits<Allocator>::pointer;
   using const_pointer =
       typename std::allocator_traits<Allocator>::const_pointer;
 
-  using reference = value_type&;
-  using const_reference = const value_type&;
+  using reference = value_type &;
+  using const_reference = const value_type &;
   using size_type = size_t;
   using difference_type = ptrdiff_t;
   using iterator = iterator;
@@ -125,67 +116,63 @@ public:
 
   using allocator_type = Allocator;
 
-  constexpr allocator_type get_allocator() const noexcept
-  {
+  constexpr allocator_type get_allocator() const noexcept {
     return m_allocator;
   };
 
   // the allocator constructor has to be no expect as well
   constexpr vector() noexcept(noexcept(Allocator())) : vector(Allocator()) {};
-  constexpr explicit vector(const Allocator& alloc) noexcept
+  constexpr explicit vector(const Allocator &alloc) noexcept
       : m_allocator{alloc} {};
-  constexpr explicit vector(size_type n, const Allocator& alloc = Allocator())
-      : m_allocator{alloc}, m_size{n}
-  {
-  }
-  constexpr vector(size_type n, const T& value,
-                   const Allocator& alloc = Allocator())
+  constexpr explicit vector(size_type n, const Allocator &alloc = Allocator())
+      : m_allocator{alloc}, m_size{n} {}
+  constexpr vector(size_type n, const T &value,
+                   const Allocator &alloc = Allocator())
       : m_allocator{alloc}, m_size{n} {};
 
   // from iterators
   template <typename InputIt>
   constexpr vector(InputIt first, InputIt last,
-                   const Allocator& alloc = Allocator())
+                   const Allocator &alloc = Allocator())
       : m_allocator{alloc} {};
 
   // template<container-compatible-range<T> R>
   // constexpr void assign_range(R&& rg);
-  constexpr vector(const vector& x) {};
-  constexpr vector(vector&& vec) noexcept {};
+  constexpr vector(const vector &x) {};
+  constexpr vector(vector &&vec) noexcept {};
 
   // type_identity_t is used so that the type of the allcoator is preserved
-  constexpr vector(const vector&, const type_identity_t<Allocator>&);
-  constexpr vector(vector&&, const type_identity_t<Allocator>&);
+  constexpr vector(const vector &, const type_identity_t<Allocator> &);
+  constexpr vector(vector &&, const type_identity_t<Allocator> &);
   constexpr vector(std::initializer_list<T> init,
-                   const Allocator& alloc = Allocator())
-      : m_allocator{alloc}
-  {
+                   const Allocator &alloc = Allocator())
+      : m_allocator{alloc} {
     m_size = init.size();
     m_capacity = m_size;
     m_data = m_allocator.allocate(m_size);
     std::uninitialized_copy(init.begin(), init.end(), m_data);
   };
 
-  constexpr vector& operator=(const vector& x);
+  constexpr vector &operator=(const vector &x);
 
   // check what this parts of allocator traits do
-  constexpr vector& operator=(vector&& x) noexcept(
+  constexpr vector &operator=(vector &&x) noexcept(
       std::allocator_traits<
           Allocator>::propagate_on_container_move_assignment::value ||
       std::allocator_traits<Allocator>::is_always_equal::value);
 
-  constexpr vector& operator=(std::initializer_list<T> init) {};
+  constexpr vector &operator=(std::initializer_list<T> init) {};
 
   reference operator[](size_type pos) {}
   const_reference operator[](size_type pos) const {}
 
   template <typename InputIt>
   constexpr void assign(InputIt first, InputIt last);
-  constexpr void assign(size_type n, const T& u);
+  constexpr void assign(size_type n, const T &u);
   constexpr void assign(std::initializer_list<T>);
 
-  constexpr void push_back(T&& value) {};
-  constexpr void push_back(const T& value) {};
+  constexpr void push_back(T &&value) {};
+  constexpr void push_back(const T &value) {};
 
   reference at(size_type pos) { return m_data[pos]; }
   const_reference at(size_type pos) const { return m_data[pos]; };
@@ -199,24 +186,20 @@ public:
   constexpr T *data() noexcept {};
   constexpr T *data() const noexcept {};
 
-  constexpr size_type max_size() const noexcept
-  {
+  constexpr size_type max_size() const noexcept {
     return std::distance(begin(), end());
   }
 
-  [[nodiscard]] constexpr bool is_empty() const noexcept
-  {
+  [[nodiscard]] constexpr bool is_empty() const noexcept {
     return m_size == 0;
   };
   [[nodiscard]] constexpr size_type size() const noexcept { return m_size; };
-  [[nodiscard]] constexpr size_type capacity() const noexcept
-  {
+  [[nodiscard]] constexpr size_type capacity() const noexcept {
     return m_capacity;
   }
 
   [[nodiscard]] constexpr bool full() const { return size() == capacity(); }
-  void grow()
-  {
+  void grow() {
     reserve(capacity() > 0 ? capacity() * m_grow_multiplier : m_new_capacity);
   };
 
@@ -224,63 +207,48 @@ public:
   constexpr void shrink_to_fit() {}
   constexpr void clear() noexcept {}
 
-  constexpr iterator insert(const_iterator pos, const T& value) {}
-  constexpr iterator insert(const_iterator pos, T&& value) {}
-  constexpr iterator insert(const_iterator pos, size_type count, const T& value)
-  {
-  }
+  constexpr iterator insert(const_iterator pos, const T &value) {}
+  constexpr iterator insert(const_iterator pos, T &&value) {}
+  constexpr iterator insert(const_iterator pos, size_type count,
+                            const T &value) {}
 
   template <typename InputIt>
-  constexpr iterator insert(const_iterator pos, InputIt first, InputIt last)
-  {
-  }
+  constexpr iterator insert(const_iterator pos, InputIt first, InputIt last) {}
 
-  constexpr iterator insert(const_iterator pos, std::initializer_list<T> init)
-  {
+  constexpr iterator insert(const_iterator pos, std::initializer_list<T> init) {
   }
 
   template <std::ranges::input_range R>
-  constexpr iterator insert_range(const_iterator pos, R&& rg)
-  {
-  }
+  constexpr iterator insert_range(const_iterator pos, R &&rg) {}
 
-  template <std::ranges::input_range R>
-  constexpr void append_range(R&& rg)
-  {
-  }
+  template <std::ranges::input_range R> constexpr void append_range(R &&rg) {}
 
   template <typename... Args>
-  constexpr iterator emplace(const_iterator pos, Args&&...args)
-  {
-  }
+  constexpr iterator emplace(const_iterator pos, Args &&...args) {}
 
   constexpr iterator erase(const_iterator pos) {}
   constexpr iterator erase(const_iterator first, const_iterator last) {}
 
   template <typename... Args>
-  constexpr reference emplace_back(Args&&...args)
-  {
-  }
+  constexpr reference emplace_back(Args &&...args) {}
 
   constexpr void pop_back() {};
   void resize(size_type count) {};
-  void resize(size_type count, const value_type& value) {}
+  void resize(size_type count, const value_type &value) {}
 
-  constexpr void swap(vector& other) noexcept {}
+  constexpr void swap(vector &other) noexcept {}
 
   template <typename I, typename Alloc>
-  constexpr void swap(std::vector<T, Alloc>& lhs,
-                      std::vector<T, Alloc>& rhs) noexcept
-  {
-  }
+  constexpr void swap(std::vector<T, Alloc> &lhs,
+                      std::vector<T, Alloc> &rhs) noexcept {}
 
   template <typename I, class Alloc, class U>
   constexpr typename std::vector<T, Alloc>::size_type
-  erase(std::vector<T, Alloc>& c, const U& value) {};
+  erase(std::vector<T, Alloc> &c, const U &value) {};
 
   template <class I, class Alloc, class Pred>
   constexpr typename std::vector<T, Alloc>::size_type
-  erase_if(std::vector<T, Alloc>& c, Pred pred) {};
+  erase_if(std::vector<T, Alloc> &c, Pred pred) {};
 
   // Fix the deduction guide!
   // template <std::ranges::input_range R,
@@ -291,8 +259,7 @@ public:
   // iterators
   constexpr Allocator getAllocator() const noexcept { return m_allocator; };
   constexpr iterator begin() noexcept { return iterator(m_data); };
-  constexpr const_iterator begin() const noexcept
-  {
+  constexpr const_iterator begin() const noexcept {
     return const_iterator(m_data);
   };
 
@@ -309,8 +276,7 @@ public:
   constexpr const_reverse_iterator crend() const noexcept;
 
   // Helpers
-  void cleanup_on_fail_aux(iterator last)
-  {
+  void cleanup_on_fail_aux(iterator last) {
     auto p{begin()};
     for (; p != last; ++p)
       std::destroy_at(p.ptr);
@@ -319,16 +285,12 @@ public:
 
   template <typename U>
     requires std::is_convertible_v<U, T>
-  void copy_aux(const U& init, size_type size)
-  {
+  void copy_aux(const U &init, size_type size) {
     auto i{begin()};
-    try
-    {
+    try {
       for (; i != begin() + size; ++i)
         std::construct_at(i.ptr, init);
-    }
-    catch (const std::exception&)
-    {
+    } catch (const std::exception &) {
       cleanup_on_fail_aux(i);
       throw;
     }
@@ -338,17 +300,13 @@ public:
   // destroy everything before the element that fails
   template <typename It>
     requires std::bidirectional_iterator<It>
-  void copy_rng_aux(It first, It last)
-  {
+  void copy_rng_aux(It first, It last) {
     auto i{begin()};
     auto j{first};
-    try
-    {
+    try {
       for (; j != last; ++i, ++j)
         std::construct_at(i.ptr, *j);
-    }
-    catch (const std::exception&)
-    {
+    } catch (const std::exception &) {
       // in the case that it fails the copy does not happen but the user does
       // not know about it
       cleanup_on_fail_aux(i);
