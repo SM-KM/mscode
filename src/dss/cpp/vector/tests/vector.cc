a5a715bce094f34ff86db8466d592c37c77e6ce2
SMKM <svargas@desma.com.mx>
2026-03-04 09:21:38 -0600

    added the test for reserve without having elements

--- src/dss/cpp/vector/tests/vector.cc
+++ src/dss/cpp/vector/tests/vector.cc
@@ -2,8 +2,27 @@
 
 #include <gtest/gtest.h>
 
+#include <memory>
+#include <stdexcept>
+
 TEST(vector, constructor) {
   dss::vector<int> vec;
   EXPECT_TRUE(vec.size() == 0);
   EXPECT_TRUE(vec.capacity() == 0);
+}
+
+TEST(vector, reserve) {
+  dss::vector<int> vec;
+  auto alloc = vec.get_allocator();
+  EXPECT_THROW(
+      vec.reserve(
+          std::allocator_traits<dss::vector<int>::allocator_type>::max_size(
+              alloc) +
+          1),
+      std::length_error);
+
+  int new_capacity = 20;
+  EXPECT_TRUE(vec.capacity() == 0);
+  vec.reserve(new_capacity);
+  EXPECT_TRUE(vec.capacity() == new_capacity);
 }
