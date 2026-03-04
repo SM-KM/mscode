253ad1022ed1d733b1a8ed184e8f0cedecd9f4de
SMKM <msantiagovargas2@gmail.com>
2026-03-04 10:32:23 -0600

    finished reserve

--- src/dss/cpp/vector/tests/vector.cc
+++ src/dss/cpp/vector/tests/vector.cc
@@ -1,5 +1,6 @@
 #include "../vector.h"
 
+#include <gtest/gtest-message.h>
 #include <gtest/gtest.h>
 
 #include <memory>
@@ -25,4 +26,16 @@ TEST(vector, reserve) {
   EXPECT_TRUE(vec.capacity() == 0);
   vec.reserve(new_capacity);
   EXPECT_TRUE(vec.capacity() == new_capacity);
+
+  // reserving for vector with existing elements
+  dss::vector<int> v;
+  v.reserve(2);
+  EXPECT_TRUE(v.capacity() == 2);
+  v.push_back(5);
+  v.push_back(6);
+  EXPECT_TRUE(v.size() == 2);
+
+  v.reserve(5);
+  EXPECT_TRUE(v.capacity() == 5);
+  EXPECT_TRUE(v.size() == 2);
 }
