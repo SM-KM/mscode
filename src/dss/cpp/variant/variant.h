ac751f265d03a8cfa40906474bc9f06f2bacb2e6
SMKM <msantiagovargas2@gmail.com>
2026-03-05 10:03:45 -0600

    other parts of the variant

--- src/dss/cpp/variant/variant.h
+++ src/dss/cpp/variant/variant.h
@@ -3,6 +3,8 @@
 
 #include <initializer_list>
 #include <utility>
+#include <variant>
+
 namespace dss {
 
 template <typename... types>
@@ -26,7 +28,82 @@ class variant {
   template <std::size_t I, class U, class... Args>
   constexpr explicit variant(std::in_place_index_t<I>,
                              std::initializer_list<U> il, Args&&... args);
+
+  constexpr ~variant();
+
+  template <class T>
+  constexpr variant& operator=(T&& t) noexcept;
+
+  constexpr std::size_t index() const noexcept;
+  constexpr bool valueless_by_exception() const noexcept;
+
+  // emplace
+  template <class T, class... Args>
+  constexpr T& emplace(Args&&... args);
+
+  template <class T, class U, class... Args>
+  constexpr T& emplace(std::initializer_list<U> il, Args&&... args);
+
+  template <std::size_t I, class... Args>
+  constexpr std::variant_alternative_t<I, variant>& emplace(Args&&... args);
+
+  template <std::size_t I, class U, class... Args>
+  constexpr std::variant_alternative_t<I, variant>& emplace(
+      std::initializer_list<U> il, Args&&... args);
+
+  // swap
+  constexpr void swap(variant& rhs) noexcept;
 };
+
+// Organize so that it can work with my variant
+template <class... Types>
+constexpr std::common_comparison_category_t<
+    std::compare_three_way_result_t<Types>...>
+operator<=>(const std::variant<Types...>& v, const std::variant<Types...>& w);
+
+template <class R, class Visitor, class... Variants>
+constexpr R visit(Visitor&& vis, Variants&&... vars);
+
+template <class T, class... Types>
+constexpr bool holds_alternative(const std::variant<Types...>& v) noexcept;
+
+// std::get(std::variant)
+template <std::size_t I, class... Types>
+constexpr std::variant_alternative_t<I, std::variant<Types...>>& get(
+    std::variant<Types...>& v);
+template <std::size_t I, class... Types>
+constexpr std::variant_alternative_t<I, std::variant<Types...>>&& get(
+    std::variant<Types...>&& v);
+template <std::size_t I, class... Types>
+constexpr const std::variant_alternative_t<I, std::variant<Types...>>& get(
+    const std::variant<Types...>& v);
+template <std::size_t I, class... Types>
+constexpr const std::variant_alternative_t<I, std::variant<Types...>>&& get(
+    const std::variant<Types...>&& v);
+
+template <class T, class... Types>
+constexpr T& get(std::variant<Types...>& v);
+template <class T, class... Types>
+constexpr T&& get(std::variant<Types...>&& v);
+template <class T, class... Types>
+constexpr const T& get(const std::variant<Types...>& v);
+template <class T, class... Types>
+constexpr const T&& get(const std::variant<Types...>&& v);
+
+template <std::size_t I, class... Types>
+constexpr std::add_pointer_t<
+    std::variant_alternative_t<I, std::variant<Types...>>>
+get_if(std::variant<Types...>* pv) noexcept;
+template <std::size_t I, class... Types>
+constexpr std::add_pointer_t<
+    const std::variant_alternative_t<I, std::variant<Types...>>>
+get_if(const std::variant<Types...>* pv) noexcept;
+
+template <class T, class... Types>
+constexpr std::add_pointer_t<T> get_if(std::variant<Types...>* pv) noexcept;
+template <class T, class... Types>
+constexpr std::add_pointer_t<const T> get_if(
+    const std::variant<Types...>* pv) noexcept;
 
 } // namespace dss
 
