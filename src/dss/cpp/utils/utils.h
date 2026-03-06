6f40a2372a569c8e4653c7cf320c03470abb227a
SMKM <msantiagovargas2@gmail.com>
2026-03-06 11:49:32 -0600

    utils for checking versions

--- src/dss/cpp/utils/utils.h
+++ src/dss/cpp/utils/utils.h
@@ -11,4 +11,7 @@ concept container_compatible_range =
     std::convertible_to<std::ranges::range_value_t<R>, T>;
 }
 
+#define GCC_VERSION \
+  (__GNUC__ * 10000 + __GNUC_MINOR__ * 100 + __GNUC_PATCHLEVEL__)
+
 #endif // UTILS_H
