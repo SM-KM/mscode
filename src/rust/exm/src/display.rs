0e711e898bf1d123b33dd8af46082868189653d4
SMKM <msantiagovargas2@gmail.com>
2026-03-25 09:53:46 -0600

    struct formatting, and display impl

--- src/rust/exm/src/display.rs
+++ src/rust/exm/src/display.rs
@@ -48,12 +48,19 @@ pub fn display() {
 }
 
 pub fn formatting() {
-    // formatting
     for city in [
         City { name: "Dublin", lat: 53.347778, lon: -6.259722 },
         City { name: "Oslo", lat: 59.95, lon: 10.75 },
         City { name: "Vancouver", lat: 49.25, lon: -123.1 },
     ] {
         println!("{}", city);
+    }
+
+    for color in [
+        Color { red: 128, green: 255, blue: 90 },
+        Color { red: 0, green: 3, blue: 254 },
+        Color { red: 0, green: 0, blue: 0 },
+    ] {
+        println!("{:?}", color);
     }
 }
