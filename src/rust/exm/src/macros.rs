31376a3cfd95c6fd52926f825216bbab5f6b27a9
SMKM <svargas@desma.com.mx>
2026-03-26 12:13:25 -0600

    macros

---
+++ src/rust/exm/src/macros.rs
@@ -0,0 +1,80 @@
+macro_rules! say_hl {
+    () => {
+        println!("hl")
+    };
+}
+
+macro_rules! create_fn {
+    ($func_name: ident) => {
+        fn $func_name() {
+            println!("Called {:?}()", stringify!($func_name));
+        }
+    };
+}
+
+macro_rules! print_res {
+    ($expression:expr) => {
+        println!("{:?} = {:?}", stringify!($expression), $expression);
+    };
+}
+
+create_fn!(foo);
+
+macro_rules! test {
+    ($left:expr; and $right:expr) => {
+        println!("{:?} and {:?} is {:?}", stringify!($left), stringify!($right), $left && $right);
+    };
+
+    ($left:expr; or $right:expr) => {
+        println!("{:?} or {:?} is {:?}", stringify!($left), stringify!($right), $left || $right);
+    };
+}
+
+macro_rules! find_min {
+    ($x:expr) => {
+        $x
+    };
+    ($x:expr, $($y:expr),+) => {
+        std::cmp::min($x, find_min!($($y),+))
+    };
+}
+
+macro_rules! assert_equal_len {
+    ($a:expr, $b:expr, $func:ident, $op:tt) => {
+        assert!(
+            $a.len() == $b.len(),
+            "{:?}: dimension mismatch: {:?} {:?} {:?}",
+            stringify!($func),
+            ($a.len(),),
+            stringify!($op),
+            ($b.len(),)
+        )
+    };
+}
+
+macro_rules! op {
+    ($func:ident, $bound:ident, $op:tt, $method:ident) => {
+        fn $func<T: $bound<T, Output = T> + Copy>(xs: &mut Vec<T>, ys: &Vec<T>) {
+            assert_equal_len!(xs, ys, $func, $op);
+            for (x, y) in xs.iter_mut().zip(ys.iter()) {
+                *x = $bound::$method(*x, *y);
+            }
+        }
+    };
+}
+
+pub fn macros() {
+    say_hl!();
+    foo();
+
+    print_res!({
+        let x = 3u32;
+        x + 2 + (x * 7)
+    });
+    test!(1i32 + 1 == 2i32; and 2i32 * 2 == 4i32);
+    test!(true; or false);
+
+    println!("{}", find_min!(1));
+    println!("{}", find_min!(1 + 2, 2));
+    println!("{}", find_min!(5, 2 * 3, 4));
+}
