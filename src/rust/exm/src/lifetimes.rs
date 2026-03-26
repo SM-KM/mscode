5fe5ab9d39fc2c50d8d4d4a2d43c704938e29cd5
SMKM <svargas@desma.com.mx>
2026-03-26 10:17:54 -0600

    mut refs wiht lifetimes

--- src/rust/exm/src/lifetimes.rs
+++ src/rust/exm/src/lifetimes.rs
@@ -3,6 +3,11 @@ fn print_refs<'a, 'b>(x: &'a i32, y: &'b i32) {
     println!("x {}, y {}", x, y);
 }
 
+// mutable references lifetimes
+fn add_one<'a>(x: &'a mut i32) {
+    *x += 1
+}
+
 pub fn lifetimes() {
     let (fo, nin) = (4, 9);
     print_refs(&fo, &nin);
