2e486163189d6d1bfd2f4a36d2bd761c63163304
SMKM <svargas@desma.com.mx>
2026-03-26 09:49:58 -0600

    phantom example

--- src/rust/exm/src/generics.rs
+++ src/rust/exm/src/generics.rs
@@ -90,12 +90,30 @@ fn difference<C: Contains>(container: &C) -> i32 {
 }
 
 // phantom
-use std::marker::PhantomData;
 
 // storage is allocated for A but not for B, so it cannot be used in
 // anything
 #[derive(PartialEq)]
 struct PhantomTuple<A, B>(A, PhantomData<B>); // B is never used
+
+use std::marker::PhantomData;
+use std::ops::Add;
+
+#[derive(Debug, Clone, Copy)]
+enum Inch {}
+#[derive(Debug, Clone, Copy)]
+enum Mm {}
+
+#[derive(Debug, Clone, Copy)]
+struct Length<Unit>(f64, PhantomData<Unit>);
+
+impl<Unit> Add for Length<Unit> {
+    type Output = Length<Unit>;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Length(self.0 + rhs.0, PhantomData)
+    }
+}
 
 pub fn ass_types() {
     let n1 = 3;
@@ -106,6 +124,14 @@ pub fn ass_types() {
     println!("Last number: {}", cont.last());
 
     println!("The difference is: {}", difference(&cont));
+
+    let one_foot: Length<Inch> = Length(12.0, PhantomData);
+    let one_meter: Length<Mm> = Length(1000.0, PhantomData);
+    let two_feet = one_foot + one_foot;
+    let two_meters = one_meter + one_meter;
+
+    println!("one foot + one_foot = {:?} in", two_feet.0);
+    println!("one meter + one_meter = {:?} mm", two_meters.0);
 }
 
 pub fn generics() {
