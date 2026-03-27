62be1480ee6a3d95868e673cb9eefd84764929c2
SMKM <msantiagovargas2@gmail.com>
2026-03-26 18:33:22 -0600

    testing iterating over results

--- src/rust/exm/src/errors.rs
+++ src/rust/exm/src/errors.rs
@@ -1,46 +1,9 @@
-use std::error;
-use std::error::Error;
-use std::fmt;
-use std::num::ParseIntError;
-
-type Result<T> = std::result::Result<T, DoubleError>;
-
-#[derive(Debug)]
-enum DoubleError {
-    EmptyVec,
-    Parse(ParseIntError),
+pub fn errors() {
+    let strings = vec!["tofu", "93", "18"];
+    let (numbers, errors): (Vec<_>, Vec<_>) =
+        strings.into_iter().map(|s| s.parse::<i32>()).partition(Result::is_ok);
+    let numbers: Vec<_> = numbers.into_iter().map(Result::unwrap).collect();
+    let errors: Vec<_> = errors.into_iter().map(Result::unwrap_err).collect();
+    println!("Numbers: {:?}", numbers);
+    println!("Errors: {:?}", errors);
 }
-
-impl fmt::Display for DoubleError {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match *self {
-            DoubleError::EmptyVec => write!(f, "please use a vector with at least one element"),
-            // The wrapped error contains additional information and is available
-            // via the source() method.
-            DoubleError::Parse(..) => write!(f, "the provided string could not be parsed as int"),
-        }
-    }
-}
-
-impl error::Error for DoubleError {
-    fn source(&self) -> Option<&(dyn error::Error + 'static)> {
-        match *self {
-            DoubleError::EmptyVec => None,
-            DoubleError::Parse(ref e) => Some(e),
-        }
-    }
-}
-
-impl From<ParseIntError> for DoubleError {
-    fn from(err: ParseIntError) -> DoubleError {
-        DoubleError::Parse(err)
-    }
-}
-
-fn double_first(vec: Vec<&str>) -> Result<i32> {
-    let first = vec.first().ok_or(DoubleError::EmptyVec)?;
-    let parsed = first.parse::<i32>()?;
-    Ok(2 * parsed)
-}
-
-pub fn errors() {}
