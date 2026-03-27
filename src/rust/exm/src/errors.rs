4d08c09e600818ab87392cd2e68a8a6c3b1d9ab3
SMKM <svargas@desma.com.mx>
2026-03-26 18:30:07 -0600

    wrapping errors

--- src/rust/exm/src/errors.rs
+++ src/rust/exm/src/errors.rs
@@ -1,121 +1,46 @@
-#![allow(dead_code)]
-
-use std::num::ParseIntError;
-#[cfg(panic = "unwind")]
-fn ah() {
-    println!("Spit it out!!!!");
-}
-
-#[cfg(not(panic = "unwind"))]
-fn ah() {
-    println!("Spit it out!!!!");
-}
-
-fn drink(beverage: Option<&str>) {
-    let inside = beverage.unwrap();
-    if inside == "lemonade" {
-        panic!("aAAaaAaaaAAAA")
-    };
-    println!("Love it, {}!", inside);
-}
-
-// option and unwrap
-fn give_adlt(drink: Option<&str>) {
-    match drink {
-        Some("limonade") => println!("too sugary"),
-        Some(inner) => println!("{}? nice", inner),
-        None => println!("Nothing?, fuk u"),
-    }
-}
-
-fn n_bit(curr_a: Option<u8>) -> Option<String> {
-    let cur: u8 = curr_a? + 1;
-    Some(format!("Next: {}", cur))
-}
-
-struct Person {
-    job: Option<Job>,
-}
-
-#[derive(Clone, Copy)]
-struct Job {
-    phone_number: Option<PhoneNumber>,
-}
-
-#[derive(Clone, Copy)]
-struct PhoneNumber {
-    area_code: Option<u8>,
-    number: u32,
-}
-
-impl Person {
-    fn work_area_code(&self) -> Option<u8> {
-        self.job?.phone_number?.area_code
-    }
-}
-
-// conbinators = map
-enum Food {
-    Apple,
-    Carrot,
-    Tomato,
-}
-
-struct Peeled(Food);
-struct Cooked(Food);
-struct Chopped(Food);
-
-fn process(food: Option<Food>) -> Option<Cooked> {
-    food.map(|f| Peeled(f)).map(|Peeled(f)| Chopped(f)).map(|Chopped(f)| Cooked(f))
-}
-
-#[derive(Debug)]
-enum Fruit {
-    Apple,
-    Orange,
-    Banana,
-    Kiwi,
-    Lemon,
-}
-
-// type AliasedResult<T> = Result<T, ParseIntError>;
-// fn mult(first: &str, second: &str) -> AliasedResult<i32> {
-//     // Early returns
-//     let _first_number = first.parse::<i32>()?;
-//     let _second_number = second.parse::<i32>()?;
-//     Ok(_first_number + _second_number)
-// }
-
 use std::error;
+use std::error::Error;
 use std::fmt;
+use std::num::ParseIntError;
 
-type Result<T> = std::result::Result<T, Box<dyn error::Error>>;
+type Result<T> = std::result::Result<T, DoubleError>;
 
 #[derive(Debug)]
-struct EmptyVec;
+enum DoubleError {
+    EmptyVec,
+    Parse(ParseIntError),
+}
 
-impl fmt::Display for EmptyVec {
+impl fmt::Display for DoubleError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "invalid first item to double")
+        match *self {
+            DoubleError::EmptyVec => write!(f, "please use a vector with at least one element"),
+            // The wrapped error contains additional information and is available
+            // via the source() method.
+            DoubleError::Parse(..) => write!(f, "the provided string could not be parsed as int"),
+        }
     }
 }
-impl error::Error for EmptyVec {}
+
+impl error::Error for DoubleError {
+    fn source(&self) -> Option<&(dyn error::Error + 'static)> {
+        match *self {
+            DoubleError::EmptyVec => None,
+            DoubleError::Parse(ref e) => Some(e),
+        }
+    }
+}
+
+impl From<ParseIntError> for DoubleError {
+    fn from(err: ParseIntError) -> DoubleError {
+        DoubleError::Parse(err)
+    }
+}
 
 fn double_first(vec: Vec<&str>) -> Result<i32> {
-    let first = vec.first().ok_or(EmptyVec)?;
+    let first = vec.first().ok_or(DoubleError::EmptyVec)?;
     let parsed = first.parse::<i32>()?;
     Ok(2 * parsed)
 }
 
-// Boxing errors
-// type Result<T> = std::result::Result<T, Box<dyn error::Error>>;
-
-pub fn errors() {
-    let numbers = vec!["42", "93", "18"];
-    let empty = vec![];
-    let strings = vec!["tofu", "93", "18"];
-
-    print!("{:?}", double_first(numbers));
-    print!("{:?}", double_first(empty));
-    print!("{:?}", double_first(strings));
-}
+pub fn errors() {}
